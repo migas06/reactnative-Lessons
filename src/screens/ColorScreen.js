8cf040956e54073d7d2de771fe917c2ff0b43626
Miguel <miguelmartins018@gmail.com>
2021-02-09 20:20:26 +0000

    color screen with random colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,13 +1,49 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useState } from "react";
+import { View, StyleSheet, Button } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 const ColorScreen = () => {
-    return (
-        <Button
-            title = 'Color'
-            onPress={() => console.log("clicked")}
-        />
-    );
+
+  const [colors, setColors] = useState([])
+
+  return (
+    <View>
+      <Button
+        title="Add new Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+
+      <FlatList
+        data={colors}
+        keyExtractor = {item => item}
+        renderItem={({item}) => {
+            return (
+              <View
+                style={{ height: 100, width: 100, backgroundColor: item }}
+              />
+            );
+          }}
+      />
+
+    </View>
+  );
+};
+
+
+const randomRgb = () => {
+
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+
 }
+
+const styles = StyleSheet.create({
+
+});
 
 export default ColorScreen;
