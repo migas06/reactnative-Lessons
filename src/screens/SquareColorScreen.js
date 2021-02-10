6efb8b32476b8dbdb70bb7d290f830db3b6d8f3f
Miguel <miguelmartins018@gmail.com>
2021-02-10 12:57:00 +0000

    improve code with validations

--- src/screens/SquareColorScreen.js
+++ src/screens/SquareColorScreen.js
@@ -2,36 +2,53 @@ import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from 'react-native';
 import ColorManagementAdapter from "../components/ColorManagementAdapter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareColorScreen = () => {
 
     const [redColor, setRedColor] = useState(0);
     const [greenColor, setGreenColor] = useState(0);
     const [blueColor, setBlueColor] = useState(0);
 
+    const setColor = (color, change) => {
+        //Color === 'red', 'green', or 'blue'
+        //Change === +15 or -15
+
+        switch (color) {
+            case 'red':
+                redColor + change > 255 || redColor + change < 0 ? null : setRedColor(redColor + change);
+                return;
+            case 'green':
+                greenColor + change > 255 || greenColor + change < 0 ? null : setGreenColor(greenColor + change);
+                return;
+            case 'blue':
+                blueColor + change > 255 || blueColor + change < 0 ? null : setBlueColor(blueColor + change);
+                return;
+            default:
+                return;
+
+        }
+    }
+
     return (
         <View>
             <ColorManagementAdapter color='Red'
-                onIncrease={() => setRedColor(redColor + 15)}
-                onDecrease={() => setRedColor(redColor - 15)} />
+                onIncrease={() => setColor('red', COLOR_INCREMENT)}
+                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} />
 
             <ColorManagementAdapter
                 color='Green'
-                onIncrease={() => setGreenColor(greenColor + 15)}
-                onDecrease={() => setGreenColor(greenColor - 15)} />
+                onIncrease={() => setColor('green', COLOR_INCREMENT)}
+                onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)} />
 
 
             <ColorManagementAdapter
                 color='Blue'
-                onIncrease={() => setBlueColor(blueColor + 15)}
-                onDecrease={() => setBlueColor(blueColor - 15)}
+                onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+                onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
             />
 
-            <View style=
-                {{
-                    height: 100,
-                    width: 100,
-                    backgroundColor: `rgb(${redColor}, ${greenColor}, ${blueColor})`
-                }}
+            <View style={{ height: 100, width: 100, backgroundColor: `rgb(${redColor}, ${greenColor}, ${blueColor})` }}
             />
         </View>
     );
