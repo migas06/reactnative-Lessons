f6569e925b8172006e8971c6d3d08e2449ef86b2
Miguel <miguelmartins018@gmail.com>
2021-02-10 22:41:25 +0000

    learn reducer concept

--- src/screens/SquareColorScreen.js
+++ src/screens/SquareColorScreen.js
@@ -1,54 +1,55 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, Button, StyleSheet } from 'react-native';
 import ColorManagementAdapter from "../components/ColorManagementAdapter";
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+    //state === { red: number, green: number, blue: number}
+    //action === {colorToChange: 'red' || 'green' || 'blue', amount 15 || -15}
+
+    switch (action.colorToChange) {
+        //by the convention
+        //case 'change_red': then (...) action.payload
+        case 'red':
+            return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : { ...state, red: state.red + action.amount };
+        case 'green':
+            return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : { ...state, green: state.green + action.amount };
+        case 'blue':
+            return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : { ...state, blue: state.blue + action.amount };
+        default:
+            return state;
+    }
+}
+
 const SquareColorScreen = () => {
 
-    const [redColor, setRedColor] = useState(0);
-    const [greenColor, setGreenColor] = useState(0);
-    const [blueColor, setBlueColor] = useState(0);
-
-    const setColor = (color, change) => {
-        //Color === 'red', 'green', or 'blue'
-        //Change === +15 or -15
-
-        switch (color) {
-            case 'red':
-                redColor + change > 255 || redColor + change < 0 ? null : setRedColor(redColor + change);
-                return;
-            case 'green':
-                greenColor + change > 255 || greenColor + change < 0 ? null : setGreenColor(greenColor + change);
-                return;
-            case 'blue':
-                blueColor + change > 255 || blueColor + change < 0 ? null : setBlueColor(blueColor + change);
-                return;
-            default:
-                return;
-
-        }
-    }
-
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+    const { red, green, blue } = state;
     return (
         <View>
             <ColorManagementAdapter color='Red'
-                onIncrease={() => setColor('red', COLOR_INCREMENT)}
-                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} />
+                                            // by the convention 
+                                            //dispatch({ type: 'change_red', payload COLOR_INCREMENT})
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })} />
 
             <ColorManagementAdapter
                 color='Green'
-                onIncrease={() => setColor('green', COLOR_INCREMENT)}
-                onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)} />
-
+                                            // by the convention 
+                                            //dispatch({ type: 'change_green', payload COLOR_INCREMENT})
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })} />
 
             <ColorManagementAdapter
                 color='Blue'
-                onIncrease={() => setColor('blue', COLOR_INCREMENT)}
-                onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+                                            // by the convention 
+                                            //dispatch({ type: 'change_blue', payload COLOR_INCREMENT})
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
             />
 
-            <View style={{ height: 100, width: 100, backgroundColor: `rgb(${redColor}, ${greenColor}, ${blueColor})` }}
+            <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }}
             />
         </View>
     );
